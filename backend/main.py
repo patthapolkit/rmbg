9a2ee1aa1da47045509bf0938f1ba702b1379df6
Patthapol Kittikun <patthapol.k@gmail.com>
2024-10-11 02:57:25 +0700

    feat: add remove background endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,51 @@
-from typing import Union
-
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, HTTPException
+from PIL import Image, UnidentifiedImageError
+from fastapi.responses import StreamingResponse
+from rembg import remove
+from io import BytesIO
+import logging
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Welcome to the Remove Background API!"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/rmbg")
+async def remove_background(file: UploadFile):
+    if file.content_type not in ["image/jpeg", "image/png"]:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid file type. Only JPEG and PNG are supported.",
+        )
 
+    try:
+        input_image = Image.open(file.file)
+    except UnidentifiedImageError:
+        raise HTTPException(status_code=400, detail="Invalid image file.")
+    except Exception as e:
+        logger.error(f"Error opening image: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error.")
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    try:
+        output_image = remove(input_image, post_process_mask=True)
+    except Exception as e:
+        logger.error(f"Error processing image: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error.")
+
+    img_io = BytesIO()
+    output_image.save(img_io, "PNG")
+    img_io.seek(0)
+
+    return StreamingResponse(
+        img_io,
+        media_type="image/png",
+        headers={
+            "Content-Disposition": f"attachment; filename={file.filename.rsplit('.', 1)[0]}_rmbg.png"
+        },
+    )
